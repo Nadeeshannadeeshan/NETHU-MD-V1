e85fedab139701f9a8769dbfc0e45fa3b33d1793
Nadeeshannadeeshan <tnadishan811@gmail.com>
2024-10-15 09:54:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,3 +17,33 @@ AUTO_VOICE: process.env.AUTO_VOICE || "true"
 
 };
 
+
+
+
+//========================= [ BOT SETTINGS ] =========================\\
+global.style = process.env.STYLE   || '5',  // put '1' to "5" here to check bot styles
+global.flush = process.env.FLUSH   || "true"; // Make it "true" if bot not responed
+global.gdbye = process.env.GOODBYE || "false"; 
+global.wlcm  = process.env.WELCOME || "false";  // Make it "false" for disable WELCOME 
+
+global.warncount = process.env.WARN_COUNT || 5,
+global.disablepm = process.env.DISABLE_PM || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG|| "false", // "true"  to see messages , "log" to open logs , "false" to hide logs messages
+global.userImages= process.env.USER_IMAGES|| "text",
+global.waPresence= process.env.WAPRESENCE ||  "set according to your need" ; // 'unavailable' | 'available' | 'composing' | 'recording' | 'paused'
+
+
+//========================= [ AUTO READ MSGS & CMDS ] =========================\\
+global.readcmds = process.env.READ_COMMAND || "false"
+global.readmessage = process.env.READ_MESSAGE || "false"
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "923135673658,923192173398";
+
+
+//========================= [ AUTO SAVE & READ STATUS ] =========================\\
+global.read_status = process.env.AUTO_READ_STATUS || "true"
+global.save_status = process.env.AUTO_SAVE_STATUS || "false"
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "923135673658,923192173398";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923135673658,923192173398";
+
+global.api_smd = "https://api-smd-1.vercel.app"
+global.scan = "https://myqr-43bb863fa5eb.herokuapp.com/";
